f76d359809c7c39c02a6d07a1e1b28cef3ad4a88
WraasTheOne <113178081+WraasTheOne@users.noreply.github.com>
2022-09-29 09:28:18 +0200

    yes

--- IMPR/Afleveringer/Aflevering2.c
+++ IMPR/Afleveringer/Aflevering2.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 
-int main(void){
+int main(void)
+{
 
-    int a,b,i,mindst;
+    int a, b, i, mindst;
 
     printf("Indtast to tal\n");
     scanf("%d %d", &a, &b);
 
-
-    if (a >=b ){
+    if (a >= b)
+    {
         mindst = b;
     }
-    else{
+    else
+    {
         mindst = a;
     }
 
-    if (a % b == 0){
+    if (a % b == 0)
+    {
         printf("%d er den største fælles devisor", b);
     }
-    else {
-        for(i = mindst; i >= 0 ; i--){
-            if(a%i == 0 && b%i == 0){
+    else
+    {
+        for (i = mindst; i >= 0; i--)
+        {
+            if (a % i == 0 && b % i == 0)
+            {
                 printf("%d er den største fælles devisor", i);
                 break;
             }
         }
     }
-    
+
     return 0;
 }
