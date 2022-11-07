a7fe14507bdb4ce2b73d8639bc7f9e940196e76e
WraasTheOne <113178081+WraasTheOne@users.noreply.github.com>
2022-11-07 08:36:02 +0100

    aflervering er nu påbegyndt //M

---
+++ IMPR/Afleveringer/aflevering3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+int main(void){
+    double a,b,h;
+    int n;
+    char (*f)(double);
+
+    printf("Vælg enten g(x) eller h(x), ved at skrive g eller h");
+    scanf("%c", &f);
+
+
+    
+
+    return 0;
+}
+
+
+double sum(){
+
+
+}
+
