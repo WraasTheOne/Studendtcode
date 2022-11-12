801f62853438e2f679671803c973637857800dd1
WraasTheOne <113178081+WraasTheOne@users.noreply.github.com>
2022-11-12 20:08:23 +0100

    k

--- IMPR/Afleveringer/eksamensopgave2.c
+++ IMPR/Afleveringer/eksamensopgave2.c
@@ -4,34 +4,37 @@
 
 #define Yatzyrunder 16
 
-int main(void){
+int scandata();
+void roll_multiple_dies(int N, int dies[]);
+
+int main(void)
+{
     int N = 0;
     N = scandata();
-    if(N < 5){
-
+    if (N < 5)
+    {
         return -1;
     }
     int point[Yatzyrunder];
     int dies[N];
-    
-
-
+    roll_multiple_dies(N, dies);
 
     return 0;
 }
 
-int scandata(){
+int scandata()
+{
     int N = 0;
-    printf("atzy with how many dies (a number less than 5 terminates)");
-    scanf("%d", N);
+    printf("Yatzy with how many dies (a number less than 5 terminates)");
+    scanf("%d", &N);
     return N;
 }
 
-
-void roll_multiple_dies(int N){
+void roll_multiple_dies(int N, int dies[])
+{
     int i;
-    for(i = 0; i < N; i++){
-
-
+    for (i = 0; i < N; i++)
+    {
+        
     }
 }
