7abe6e15e318bdcecf9893bfa14b72e88c3dce56
WraasTheOne <113178081+WraasTheOne@users.noreply.github.com>
2022-12-05 14:47:52 +0100

    hej

---
+++ IMPR/Afleveringer/eksamen3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+typedef struct{
+    char ugedag[4];
+    char dato[6];
+    char hold_1[4];
+    char hold_2[4];
+    int scorer_hold_hjem;
+    int corer_hold_ude;
+    int tilskuertal;
+}KAMP;
+
+typedef struct {
+    char hold_navn[4];
+    int point;
+    int mål_scoret;
+    int mål_gået_ind;
+}HOLD;
+
+int main(void){
+    KAMP kamp_array[1];
+    
+
+
+
+
+    return 0;
+}
